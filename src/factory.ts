ac2561fe5c7ed55d1c572f66e945d9a3628ff267
Jeff Ching <chingor@google.com>
2021-11-02 14:59:05 -0700

    factory handles go-yoshi

--- src/factory.ts
+++ src/factory.ts
@@ -14,6 +14,7 @@
 
 import {Strategy, StrategyOptions} from './strategy';
 import {Go} from './strategies/go';
+import {GoYoshi} from './strategies/go-yoshi';
 import {JavaYoshi} from './strategies/java-yoshi';
 import {KRMBlueprint} from './strategies/krm-blueprint';
 import {OCaml} from './strategies/ocaml';
@@ -66,6 +67,7 @@ type ReleaseBuilder = (options: StrategyOptions) => Strategy;
 type Releasers = Record<string, ReleaseBuilder>;
 const releasers: Releasers = {
   go: options => new Go(options),
+  'go-yoshi': options => new GoYoshi(options),
   'krm-blueprint': options => new KRMBlueprint(options),
   node: options => new Node(options),
   ocaml: options => new OCaml(options),
